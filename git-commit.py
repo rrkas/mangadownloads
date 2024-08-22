c9f1fbdc44f627f923aba4cfaed3bc3f0f1141b4
Rohnak Agarwal <rrka79wal@gmail.com>
2024-08-22 10:43:24 +0530

    regular update: flushing

--- git-commit.py
+++ git-commit.py
@@ -41,7 +41,8 @@ for dir_path in sorted(glob.glob("./m*/*/imgs/*/")):
         + f'git commit -m "Updated: {dir_path.lstrip("./")}" && '
         + "git push origin main",
     )
-    # os.system(f"sudo rm -rf {dir_path}")
+    os.system(f"sudo rm -rf {dir_path}")
+    os.system(f"sudo rm -rf {dir_path.replace('imgs/', 'pdfs/')}")
 
 os.system(
     "git add *.py .gitignore && "
